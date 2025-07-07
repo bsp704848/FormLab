5165a8d88fa64eca93106134b9da366a777a2893
bsp704848 <bsp704848@gmail.com>
2025-07-07 13:01:42 +0530

    modified:   src/utils/authApi.js

--- src/utils/authApi.js
+++ src/utils/authApi.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
 export const loginUser = async (payload) => {
-    const response = await axios.post(`${API_URL}/login`, payload) 
+    const response = await axios.post(`${API_URL}/login`, payload, { withCredentials: true }) 
     return response.data
 } 
 
 export const registerUser = async (payload) => {
-    const response = await axios.post(`${API_URL}/register`, payload)
+    const response = await axios.post(`${API_URL}/register`, payload, { withCredentials: true })
     return response.data
 }
